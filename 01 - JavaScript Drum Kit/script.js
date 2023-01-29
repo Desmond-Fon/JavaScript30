ab3af370e307d63dddd4db1cdb5c9dfe29a1269e
desmondfon <desmondfon@yahoo.com>
2023-01-30 00:34:58 +0100

    implemented the function for removing transition

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,14 +1,20 @@
+const keys = document.querySelectorAll('.key')
+// console.log("working");
 
-console.log("working");
+function removeTransition (e) {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing')
+}
 
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
+function playSound (e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-  key.classList.add("playing");
-  audio.currentTime = 0;
-  audio.play();
+    key.classList.add('playing')
+    audio.currentTime = 0;
+    audio.play()
 }
 
 window.addEventListener("keydown", playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
